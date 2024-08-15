2721bf949f7a117ce73e91404772b73bb2de61ac
guilherme <guilherme@acheipneus.com.br>
2024-08-14 23:10:32 -0300

    prepare sql statement with named columns

--- InserirAluno.php
+++ InserirAluno.php
@@ -6,13 +6,13 @@ require_once 'vendor/autoload.php';
 $dbPath = __DIR__ . '/banco.sqlite';
 $pdo = new PDO('sqlite:' . $dbPath);
 
-$student = new Student(null, 'bob', new \DateTimeImmutable('1999-11-11'));
+$student = new Student(null, 'jane', new \DateTimeImmutable('1998-11-11'));
 
-$sqlInsert = "INSERT INTO students (name, birth_date) VALUES (?, ?);";
+$sqlInsert = "INSERT INTO students (name, birth_date) VALUES (:name, :birth_date);";
 
 $statement = $pdo->prepare($sqlInsert);
-$statement->bindValue(1, $student->name());
-$statement->bindValue(2, $student->birthDate()->format('Y-m-d'));
+$statement->bindValue(':name', $student->name());
+$statement->bindValue(':birth_date', $student->birthDate()->format('Y-m-d'));
 
 if ($statement->execute()) {
     echo "Inserido com sucesso!";
