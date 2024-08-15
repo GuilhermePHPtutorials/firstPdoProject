13289d85e282da2254625771999f13350eec9144
guilherme <guilherme@acheipneus.com.br>
2024-08-14 22:06:45 -0300

    testing sqlite3 connection

---
+++ README.md
@@ -0,0 +1,4 @@
+### requisitos
+PHP >= 7.4
+sqlite3
+php-sqlite3
--- conexao.php
+++ conexao.php
@@ -4,3 +4,5 @@ $caminhoBanco = __DIR__ . '/banco.sqlite';
 $pdo = new PDO('sqlite:' . $caminhoBanco);
 
 echo 'Conectei';
+
+$pdo->exec('CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY, name TEXT, birth_date TEXT);');
