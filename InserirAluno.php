abd786cc276f54852624e32067d627f54c8cbf36
guilherme <guilherme@acheipneus.com.br>
2024-08-14 23:07:12 -0300

    insert with sql statement to avoid sql injection

--- InserirAluno.php
+++ InserirAluno.php
@@ -6,9 +6,14 @@ require_once 'vendor/autoload.php';
 $dbPath = __DIR__ . '/banco.sqlite';
 $pdo = new PDO('sqlite:' . $dbPath);
 
-$student = new Student(null, 'Guilherme', new \DateTimeImmutable('1900-11-11'));
+$student = new Student(null, 'bob', new \DateTimeImmutable('1999-11-11'));
 
-$sqlInsert = "INSERT INTO students (name, birth_date) VALUES ('{$student->name()}', '{$student->birthDate()->format('Y-m-d')}');";
-echo $sqlInsert;
+$sqlInsert = "INSERT INTO students (name, birth_date) VALUES (?, ?);";
 
-var_dump($pdo->exec($sqlInsert));
+$statement = $pdo->prepare($sqlInsert);
+$statement->bindValue(1, $student->name());
+$statement->bindValue(2, $student->birthDate()->format('Y-m-d'));
+
+if ($statement->execute()) {
+    echo "Inserido com sucesso!";
+}
